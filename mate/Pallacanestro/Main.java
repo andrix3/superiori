2f1cd55ec5f46bef3dd5d25803327a89194e634f
TanogT <116196238+TanogT@users.noreply.github.com>
2022-11-01 23:07:46 +0100

    Update Main.java

--- mate/Pallacanestro/Main.java
+++ mate/Pallacanestro/Main.java
@@ -1,9 +1,21 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		Frame fr = new Frame();
+		
+		Thread.sleep(2000);
+		
+		//per spostare la palla
+		for(int i = 0; i < 500; i++) {
+			fr.spostaPalla(i, 50);
+			Thread.sleep(1);
+		}
 	}
-
+	
+	//Nella tua superclasse:
+	public void giocaBut(int forza, int angolo){
+		
+	}
 }
