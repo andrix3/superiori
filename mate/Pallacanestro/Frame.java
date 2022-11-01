473b3c94ab5b559727b3147a1bf41b15d297affa
TanogT <116196238+TanogT@users.noreply.github.com>
2022-11-01 23:07:23 +0100

    Update Frame.java

--- mate/Pallacanestro/Frame.java
+++ mate/Pallacanestro/Frame.java
@@ -2,17 +2,24 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Label;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
-public class Frame{
+public class Frame extends Main{
+	final String pathPalla = "C:\\Users\\gaeta\\Documents\\java\\Pallacanestro\\palla.png";
+	final String pathCanestro = "C:\\Users\\gaeta\\Documents\\java\\Pallacanestro\\canestro.jpg";
 	
-	final int LARGHEZZA = 500;
-	final int ALTEZZA = 400;
+	final int LARGHEZZA = 800;
+	final int ALTEZZA = 500;
 	
 	private int forza;
 	private int angolo;
@@ -38,14 +45,26 @@ public class Frame{
 	JSlider slider2 = new JSlider(0, 100, 50);
 	
 	JPanel panel2 = new JPanel();
+	JLabel label3 = new JLabel();
+	JLabel label4 = new JLabel();
+	
+	JButton button = new JButton();
+	
+	
+	ImageIcon ic1 = new ImageIcon(pathPalla);
+    ImageIcon palla = new ImageIcon(ic1.getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH));
+    
+    ImageIcon ic2 = new ImageIcon(pathCanestro);
+    ImageIcon canestro = new ImageIcon(ic2.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
 
-	public Frame() {
+	public Frame() throws InterruptedException {
 		setSlider();
 		setLabel();
+		setButton();
 		setPanel();
 		setFrame();
 	}
-	private void setFrame() {
+	private void setFrame(){
 		frame.setTitle("Pallacanestro");
 		frame.setSize(LARGHEZZA, ALTEZZA);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,6 +74,7 @@ public class Frame{
 		frame.getContentPane().add(panel2, BorderLayout.CENTER);
 
 		frame.setVisible(true);
+		
 	}
 	
 	private void setPanel() {
@@ -63,17 +83,21 @@ public class Frame{
 		
 		panel1.add(label1);
 		panel1.add(slider1);
-		
+		panel1.add(Box.createRigidArea(new Dimension(0,40)));		//separatore
 		panel1.add(label2);
 		panel1.add(slider2);
+		panel1.add(Box.createRigidArea(new Dimension(0,40)));		//separatore
+		panel1.add(button);
 		
 		panel1.setMaximumSize(null);
 		
 		panel2.setBorder(BorderFactory.createLineBorder(Color.black));
 		panel2.setLayout(null);
+		panel2.add(label3);
+		panel2.add(label4);
 	}
 	
-	private void setLabel() {
+	private void setLabel(){
 		label1.setBounds(0, 30, 120, 50);
 		label1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		label1.setText("Forza: " + slider1.getValue());
@@ -83,6 +107,22 @@ public class Frame{
 		label2.setAlignmentX(Component.CENTER_ALIGNMENT);
 		label2.setPreferredSize(new Dimension(120, 30));
 		label2.setText("Angolo: " + slider2.getValue());
+		label2.setLayout(null);
+		
+		label3.setBounds(100, 100, 45, 45);
+		label3.setIcon(palla);
+		
+		label4.setBounds(350, 250, 150, 150);
+		label4.setIcon(canestro);
+	}
+	
+	private void setButton() {
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.setText("Gioca");
+		button.setAlignmentY(150);
+		button.setPreferredSize(new Dimension(120, 30));
+		button.setFocusPainted(false);
+		button.addActionListener(e -> giocaBut());
 	}
 	
 	private void setSlider() {
@@ -109,5 +149,13 @@ public class Frame{
 	public void cambiaAngolo() {
 		label2.setText("Angolo: " + slider2.getValue());
 		setAngolo(slider2.getValue());
+	}
+	
+	public void giocaBut() {
+		super.giocaBut(getForza(), getAngolo());
+	}
+	
+	public void spostaPalla(int x, int y) {
+		label3.setBounds(x, y, 45, 45);
 	}
 }
